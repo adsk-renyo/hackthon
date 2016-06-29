500252f97e8d8c02caea12536599c7d5ce1ae952
Yongfu <yongfu@adskrenyo.ads.autodesk.com>
2016-06-29 15:01:17 -0400

    optimize mobile fetch to only get thumbnail if it does not exist

--- mobileClient/index.ios.js
+++ mobileClient/index.ios.js
@@ -18,6 +18,8 @@ import {
 //var TimerMixin = require('react-timer-mixin');
 import TimerMixin from 'react-timer-mixin';
 
+var apiBaseUrl = 'http://localhost:3000';//'https://1e22ea73.ngrok.io';
+
 class Designs  extends Component{
   constructor(props, context) {
     super(props, context);
@@ -36,32 +38,47 @@ class Designs  extends Component{
 
   _trieveData() {
     var self = this;
-    fetch('http://localhost:3000/api/getBucketObjects', {
+
+   fetch(apiBaseUrl + '/api/getBucketObjects', {
+      body:null,
       method: 'GET',
       headers: {
         'Accept': 'application/json',
       },
     }).then((response) => response.text())
         .then((responseText) => {
+          var existingSectionIdentity = this.state.dataSource.sectionIdentities[0];
+          var existingArr = this.state.dataSource._dataBlob[existingSectionIdentity];
+          var existingObjs = {};
+          existingArr.forEach(function(obj) {
+            existingObjs[obj.objectId] = obj;
+          });
           var objs = JSON.parse(responseText);
-          var proms = objs.map(function(obj) {
+
+          var proms = objs.map(function(obj) {            
             var oid = obj.objectId;
-            var url = 'http://localhost:3000/api/getThumbnail/'+encodeURIComponent(oid);
-            return fetch(url, {method:'GET', headers:{'Accept': 'application/json'}}).then((response1) => response1.text()).then(
-              (responseText1) => {obj.base64 = responseText1; return obj});
+            var existingObj = existingObjs[oid];
+            if(existingObj && existingObj.base64 && existingObj.base64.length > 0) return Promise.resolve(existingObj);
+            var url = apiBaseUrl + '/api/getThumbnail/'+encodeURIComponent(oid);
+            return fetch(url, {body:null, method:'GET', headers:{'Accept': 'application/json'}}).then((response1) => response1.text()).then(
+              (responseText1) => {
+                console.log('thumbnail for url', oid);
+
+                obj.base64 = responseText1; 
+                return obj;
+              });
           }); //forEach
           Promise.all(proms).then(values=>{
             var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.objectId !== r2.objectId});
             var dd = ds.cloneWithRows(values);   
             self.setState({dataSource:dd});
-            console.log('aaa');
           });
 
     })
+
   }
 
   render () {
-    console.log('Designs data:', this.state.dataSource);
     return (
     <View>
       <Text style={styles.count}> # of designs in your project: {this.state.dataSource._cachedRowCount} </Text>
