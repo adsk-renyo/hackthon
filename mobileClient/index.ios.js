c08d4526bd3b2bd18e30f88a7a8bee168fdab209
Yongfu <yongfu@adskrenyo.ads.autodesk.com>
2016-07-01 09:27:44 -0400

    update mobile client

--- mobileClient/index.ios.js
+++ mobileClient/index.ios.js
@@ -12,20 +12,24 @@ import {
   Image,
   View,
   ListView,
-  TouchableHighlight
+  TouchableHighlight,
+  Navigator,
+  TouchableOpacity,
+  TextInput
 } from 'react-native';
 
 //var TimerMixin = require('react-timer-mixin');
 import TimerMixin from 'react-timer-mixin';
 
-var apiBaseUrl = 'https://0c575ac1.ngrok.io';
+var apiBaseUrl = 'https://988ad979.ngrok.io';
+var base64ImageHeader = 'data:image/png;base64,';
 
 class Designs  extends Component{
   constructor(props, context) {
     super(props, context);
     var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     var dd = ds.cloneWithRows([]);
-    this.state = {dataSource: dd, errMsg:''};
+    this.state = {dataSource: dd, allDesigns:[], errMsg:'', searchText:'', searchResults:undefined};
   };
 
   componentDidMount() {
@@ -64,7 +68,7 @@ class Designs  extends Component{
           var proms = objs.map(function(obj) {            
             var oid = obj.objectId;
             var existingObj = existingObjs[oid];
-            if(existingObj && existingObj.base64 && existingObj.base64.length > 0) return Promise.resolve(existingObj);
+            if(existingObj && existingObj.base64 && existingObj.base64.length > base64ImageHeader.length + 1) return Promise.resolve(existingObj);
             obj.date = Date.now();
             obj.key = obj.objectKey;
 
@@ -83,13 +87,55 @@ class Designs  extends Component{
               else if(v1.date > v2.date) return -1;
               else return 1;
             });
-            var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.objectId !== r2.objectId});
-            var dd = ds.cloneWithRows(values);   
-            self.setState({dataSource:dd});
+            self.setState({allDesigns: values.slice(0)});
+            self._filterBySearch();
           });
-
     })
+  }
 
+  _filterBySearch() {
+      var searchResults = this.state.searchResults;
+      var filtered = this.state.allDesigns;
+      if(this.state.searchText && this.state.searchText.length > 0 && searchResults) {
+        filtered = filtered.filter((item)=>{
+          var bFound = false;
+          for(let i=0; i<searchResults.length; ++i) {
+            var expObj = searchResults[i];
+            if(item.objectKey == expObj.file) {
+              bFound = true;
+              break;
+            }
+          }
+          return bFound;
+        });
+      }
+
+      var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.objectId !== r2.objectId});
+      var dd = ds.cloneWithRows(filtered);   
+      this.setState({dataSource:dd});
+  }
+
+  _handleSearchPress() {
+    var text = this.state.searchText;
+    var num = parseInt(text);
+    console.log('search text: ', num);
+    if(text.length <= 0 || num<= 0 || num === NaN) {
+      this.setState({searchResults:undefined});
+      this._filterBySearch();
+      return;
+    }
+    fetch(apiBaseUrl + '/api/fetch/' + num, {
+      body:null,
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      },
+    }).then((response1) => response1.text()).then((txt)=>{
+      var searchResults = JSON.parse(txt);
+      this.setState({searchResults:searchResults});
+      console.log(txt);
+      this._filterBySearch();
+    });
   }
 
   render () {
@@ -99,8 +145,24 @@ class Designs  extends Component{
     } else {
       msg = '# of designs in your project:' + this.state.dataSource._cachedRowCount;
     }
+      // <TouchableOpacity onPress={()=>this._handleSearchPress()}>
+      // 	<Text style={styles.button}>Search</Text>
+      // </TouchableOpacity>     
+
     return (
     <View style={styles.listViewWrapper}>
+ 
+      <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft:10}}
+        onChangeText={(text) => {
+          this.setState({searchText:text});
+          this._handleSearchPress();
+        }}
+        value={this.state.text}
+        keyboardType='number-pad'
+        placeholder='max # triangles'
+      />
+    
       <Text style={styles.count}> {msg} </Text>
       <ListView
         style={styles.listViewContainer}
@@ -108,7 +170,6 @@ class Designs  extends Component{
         renderRow={(rowData, sectionID, rowID) => 
           <TouchableHighlight onPress={() => {
             this._pressRow(rowID);
-            //highlightRow(sectionID, rowID);
           }}>
             <View>
               <View style={styles.row}>
@@ -126,6 +187,10 @@ class Designs  extends Component{
   }
 
   _pressRow(rowID) {
+    var existingSectionIdentity = this.state.dataSource.sectionIdentities[0];
+    var existingArr = this.state.dataSource._dataBlob[existingSectionIdentity];
+    var activeDesign =  existingArr[rowID];   
+    this.props.navigator.push({id:'DesignDetailPage', activeDesign: JSON.stringify(activeDesign)});
     console.log('clickd row: ', rowID);
   }
   _renderHeader() {
@@ -148,24 +213,93 @@ class Designs  extends Component{
 class mobileClient extends Component {
   constructor(props, context) {
     super(props, context);
+  }
 
-    this.state = {
-      token: '',
-      bucketObjects: []
-    };
-  };
+  render() {
+    return (<Navigator
+      initialRoute={{id:"DesignListView"}}
+      renderScene={this._navRenderScene}
+    />);
+  }
 
-  componentDidMount() {
+  _navRenderScene(route, navigator) {
+    switch(route.id) {
+      case "DesignListView":
+        return(<DesignListView navigator={navigator}></DesignListView>);
+      case "DesignDetailPage":
+        return(<DesignDetailPage navigator={navigator}></DesignDetailPage>);
+      default:
+        return(<DesignListView navigator={navigator}></DesignListView>);
+    }
+  }
+}
+
+class DesignListView extends Component {
+  constructor(props, context) {
+    super(props, context);
   }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Autodesk Hackthon Project Viewer
+          Hackthon Project Viewer
         </Text>
-        <Designs></Designs>
+        <Designs navigator={this.props.navigator} ></Designs>
       </View>
     );
+  }
+};
+
+class DesignDetailPage extends Component {
+  constructor(props, context) {
+    super(props, context);
+    var dataObj = this.props.navigator.navigationContext.currentRoute;
+    var activeDesign = JSON.parse(dataObj.activeDesign);    
+    this.state = {activeDesign: activeDesign, meshProps:{}};
+  }
+  componentDidMount() {
+    var activeDesign = this.state.activeDesign;
+    
+    fetch(apiBaseUrl + '/api/getMeshProps/' + activeDesign.objectKey, {
+      body:null,
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      },
+    }).then((response1) => response1.text()).then((txt)=>{
+      try {
+        var meshProps = JSON.parse(txt);
+        this.setState({meshProps:meshProps});
+        console.log('mesh props', meshProps);
+      } catch(e) {
+        console.log(e);
+      }
+
+    });
+  }
+
+  _handlePress() {
+    this.props.navigator.pop();
+  }
+  render() {
+    var activeDesign = this.state.activeDesign;
+    var propsNode = [];
+    var meshProps = this.state.meshProps;
+    for(var keyName in meshProps) {
+      if(meshProps.hasOwnProperty(keyName)) {
+        propsNode.push(<Text style={styles.bucketKey} key={keyName}>{keyName} : {meshProps[keyName]}</Text>)
+      }
+    }
+    return (
+      <View style={[styles.container]}>
+        <Text style={styles.welcome}>{activeDesign.objectKey}</Text>
+        <Image style={styles.thumbBig} source={{uri: activeDesign.base64}} />
+        {propsNode}
+        <TouchableOpacity onPress={()=>this._handlePress()}>
+            <Text style={styles.button}>Go back</Text>
+        </TouchableOpacity>
+       </View>
+    )
   }
 }
 
@@ -207,6 +341,11 @@ const styles = StyleSheet.create({
     width: 64,
     height: 64,
   },
+  thumbBig: {
+    width: 100,
+    height: 100,
+    margin:10,
+  },
   text: {
     flex: 1,
     justifyContent: 'center',
@@ -218,6 +357,13 @@ const styles = StyleSheet.create({
     margin: 10,
     marginBottom:20
   }, 
+  button: {
+    backgroundColor: 'grey',
+    fontSize: 16,
+    textAlign: 'center',
+    justifyContent: 'center',
+    margin: 10,    
+  }
 });
 
 AppRegistry.registerComponent('mobileClient', () => mobileClient);
